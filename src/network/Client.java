3d14e527204e76a334b1bb2c2e4a3d8ebc989abb
guillaumearnx <37373941+guillaumearnx@users.noreply.github.com>
2021-07-21 01:16:22 +0200

    Update Client.java

--- src/network/Client.java
+++ src/network/Client.java
@@ -1,62 +1,90 @@
 package network;
 
+import game.Partie;
+import panels.ClientPanel;
+
+import javax.swing.*;
+import java.awt.*;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.nio.Buffer;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Client {
 
-    public static void main(String[] args) throws IOException
-    {
-        try
-        {
-            Scanner scn = new Scanner(System.in);
 
+
+    final String IP = "192.168.0.4";
+    final int PORT = 6587;
+
+    private Partie p;
+
+    public Client(String name) {
+
+        try {
             // getting localhost ip
-            InetAddress ip = InetAddress.getByName("localhost");
+            InetAddress ip = InetAddress.getByName(IP);
 
             // establish the connection with server port 5056
-            Socket s = new Socket(ip, 6587);
+            Socket s = new Socket(ip, PORT);
 
             // obtaining input and out streams
             DataInputStream dis = new DataInputStream(s.getInputStream());
+            ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
             DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            try {
+                System.out.println("Reception partie");
+                p = (Partie) ois.readObject();
+                System.out.println("Envoi nom");
+                dos.writeUTF(name);
+
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            //FRAME
+            ClientPanel cp = new ClientPanel(this, name);
+            JFrame f = new JFrame();
+            f.setTitle("JavaNo - Client");
+            cp.setPreferredSize(new Dimension(1400, 800));
+            f.setContentPane(cp);
+            f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            f.requestFocusInWindow();
+            f.pack();
+            f.setVisible(true);
 
             // the following loop performs the exchange of
             // information between client and client handler
-            while (true)
-            {
-                System.out.println(dis.readUTF());
-                String tosend = scn.nextLine();
-                dos.writeUTF(tosend);
+            new Thread(() -> {
 
-                // If client sends exit,close this connection
-                // and then break from the while loop
-                if(tosend.equals("Exit"))
-                {
-                    System.out.println("Closing this connection : " + s);
-                    s.close();
-                    System.out.println("Connection closed");
-                    break;
+                System.out.println("Client : " + name + " started..");
+                while (true){
+                    try {
+                        String action = dis.readUTF();
+                        switch (action){
+                            case "repaint":
+                                p = (Partie) ois.readObject();
+                                cp.repaint();
+                                break;
+                        }
+                    }catch (IOException | ClassNotFoundException e){
+                        System.out.println("Erreur");
+                        System.exit(-1);
+                    }
                 }
-
-                // printing date or time as requested by client
-                String received = dis.readUTF();
-                System.out.println(received);
-            }
+            }).start();
 
             // closing resources
-            scn.close();
-            dis.close();
-            dos.close();
-        }catch(Exception e){
+            /*dis.close();
+            dos.close();*/
+        } catch (
+                Exception e) {
             e.printStackTrace();
         }
+
+
     }
 
+    public Partie getPartie() {
+        return p;
+    }
 }
 
