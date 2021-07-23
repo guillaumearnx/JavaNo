8c520ed36f0106efe52b8ed8e6af35b4d32ead34
ARNOUX Guillaume <arnouxguillaume54@hotmail.fr>
2021-07-23 23:53:22 +0200

    Create SerialImage.java

---
+++ src/datatransfer/SerialImage.java
@@ -0,0 +1,27 @@
+package datatransfer;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+
+public class SerialImage extends BufferedImage implements Serializable {
+
+    public SerialImage(File readFile) throws IOException {
+        this(ImageIO.read(readFile));
+    }
+
+    public SerialImage(BufferedImage image){
+        this(image.getColorModel(), image.getRaster());
+    }
+
+    public SerialImage(ColorModel cm, WritableRaster wr){
+        super(cm, wr, cm.isAlphaPremultiplied(), null);
+    }
+
+    public boolean write(String format, File output) throws IOException {
+        return ImageIO.write(this, format, output);
+    }
+}
