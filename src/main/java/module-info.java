8126675cad1d7acdfde9e116d26eb613e9eef230
ARNOUX Guillaume <arnouxguillaume54@hotmail.fr>
2021-11-29 18:41:20 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,5 +6,12 @@ module fr.arnoux23u.javano {
     requires java.desktop;
 
     opens fr.arnoux23u.javano to javafx.fxml;
+    opens fr.arnoux23u.javano.mvc.views to javafx.fxml;
     exports fr.arnoux23u.javano;
+    exports fr.arnoux23u.javano.data;
+    exports fr.arnoux23u.javano.mvc;
+    exports fr.arnoux23u.javano.mvc.controllers;
+    exports fr.arnoux23u.javano.mvc.views;
+    exports fr.arnoux23u.javano.game;
+    exports fr.arnoux23u.javano.network;
 }
